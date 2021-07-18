7c73720391ba220d689faac4166650aca74f8349
AlmubdyMutaikhan <71497569+AlmubdyMutaikhan@users.noreply.github.com>
2021-07-18 17:11:34 +0600

    Update oAuth.rote.js

--- routes/oAuth.rote.js
+++ routes/oAuth.rote.js
@@ -8,6 +8,7 @@ oAuth.get('/google', passport.authenticate('google', {scope : ["profile", "email
 
 oAuth.get('/google/callback', passport.authenticate('google', {failureRedirect : '/google/failed'}), 
     (req, res) => {
+ alert("here is good")
         console.log(req)
         // res.redirect()
         res.json({ "msg" : "unsucessful authorized", "success" : false})
