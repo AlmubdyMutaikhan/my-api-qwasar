2b863ad17d1eab44ac2c5c9b7ab3ab88da317183
Mutaikhan Almubdi <mutaikhan.almubdy@gmail.com>
2021-07-20 01:41:47 +0600

    update swagger

--- api-documentation-swagger/servers.js
+++ api-documentation-swagger/servers.js
@@ -1,6 +1,10 @@
 module.exports = {
     servers: [
       {
+        url: "http://myapi-env.eba-ip377w6m.us-east-2.elasticbeanstalk.com/", // url
+        description: "AWS server", // name
+      },
+      {
         url: "http://localhost:8080/", // url
         description: "Local server", // name
       },
