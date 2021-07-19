f49a9322662307d31b71b71e393c11fbe83c65a3
AlmubdyMutaikhan <71497569+AlmubdyMutaikhan@users.noreply.github.com>
2021-07-19 22:42:13 +0600

    Update servers.js

--- api-documentation-swagger/servers.js
+++ api-documentation-swagger/servers.js
@@ -3,10 +3,6 @@ module.exports = {
       {
         url: "http://localhost:8080/", // url
         description: "Local server", // name
-      },
-      {
-        url: "http://ec2-18-119-17-184.us-east-2.compute.amazonaws.com/", // url
-        description: "Production server", // name
       }
     ],
   };
