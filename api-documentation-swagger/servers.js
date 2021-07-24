cdeb6d7ca0c1cb6fe7be7f7552e1178d47b6ea66
Mutaikhan Almubdi <mutaikhan.almubdy@gmail.com>
2021-07-25 01:08:53 +0600

    add to swagger new server name

--- api-documentation-swagger/servers.js
+++ api-documentation-swagger/servers.js
@@ -1,6 +1,10 @@
 module.exports = {
     servers: [
       {
+        url: "http://ec2-18-116-88-142.us-east-2.compute.amazonaws.com/", // url
+        description: "AWS server", // name
+      },
+      {
         url: "http://localhost:8080/", // url
         description: "Local server", // name
       },
